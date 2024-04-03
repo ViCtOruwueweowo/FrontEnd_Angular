574b70ab08ae346c75f147a608ce1e53c92f2f83
Victor_99 <vamonarrez21@gmail.com>
2024-04-03 13:16:35 -0600

    a

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 import { CanActivateFn, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 import { Router, RouterModule } from '@angular/router';
+import { ConstructorProvider } from '@angular/core';
 Injectable({
   providedIn: 'root'
 })
 
+
+
 export const authGuard: CanActivateFn = (route, state) => {
+  let router = new Router();
+
   const token = localStorage.getItem('mi_token'); // Obtiene el token del localStorage
 
   console.log('Token obtenido del localStorage:', token); // Imprime el token
@@ -15,11 +20,11 @@ export const authGuard: CanActivateFn = (route, state) => {
     // Si el token existe, permite la navegación
     return true;
   } else {
-    console.log('Paso por false'); 
+    console.log('Paso por false');
 
     // Si no hay token, redirige al usuario a la página de inicio de sesión (o cualquier otra página que desees)
     // Asegúrate de tener una instancia de Router disponible para usarla aquí
-    this.router.navigate(['/']);
+    router.navigate(['/']);
     return false;
   }
 };
